74548120fea396b8fb38c1637162ccf278a3dbd0
Zoltan Halasz <zoltan.halasz@outlook.com>
2023-12-16 19:49:02 +0200

    fix

---
+++ WeatherForecast/Function1.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace WeatherForecast
+{
+    public static class WeatherFunction
+    {
+        [FunctionName("WeatherFunction")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var randomNumber = new Random();
+            var temp = 0;
+
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = temp = randomNumber.Next(-20, 55),
+                Summary = GetSummary(temp)
+            }).ToArray();
+
+            return new OkObjectResult(result);
+        }
+        private static string GetSummary(int temp)
+        {
+            var summary = "Mild";
+            if (temp >= 32)
+            {
+                summary = "Hot";
+            }
+            else if (temp <= 16 && temp > 0)
+            {
+                summary = "Cold";
+            }
+            else if (temp <= 0)
+            {
+                summary = "Freezing";
+            }
+            return summary;
+        }
+
+        public class WeatherForecast
+        {
+            public DateOnly Date { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}

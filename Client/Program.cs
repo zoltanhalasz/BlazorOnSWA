ad7aa18d7cd1598e46e239af70907dbd00cf5165
Zoltan Halasz <zoltan.halasz@outlook.com>
2023-12-16 19:26:39 +0200

    ADD BACKEND API FCT

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,9 +9,8 @@ namespace Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.RootComponents.Add<HeadOutlet>("head::after");            
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
